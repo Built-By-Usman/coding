d5547fea4662b7df19de6f062325f71a5297c7a9
usman <builtbyusman@gmail.com>
2026-02-11 00:02:21 +0500

    updated

--- FastApi/Practice Project/main.py
+++ FastApi/Practice Project/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI,Depends
 from schemas import UserSchema
-from sqlalchemy.orm import session
+from sqlalchemy.orm import Session
 from database import base,engine,get_db
 from models import User
 
 app=FastAPI()
 
-base.metadata.create_all(bind=engine)
+base.metadata.create_all(engine)
 
 
 
@@ -15,8 +15,8 @@ def home():
     return {"detail":"Hello world"}
 
 @app.post('/user',response_model=UserSchema)
-def create_user(user:UserSchema,db:session=Depends(get_db)):
-    user=User(name=user.name,age=user.age)
+def create_user(user:UserSchema,db:Session=Depends(get_db)):
+    user = User(name=user.name,age=user.age)
     db.add(user)
     db.commit()
     db.refresh(user)
@@ -25,3 +25,7 @@ def create_user(user:UserSchema,db:session=Depends(get_db)):
 @app.get('/get/{id}')
 def user_with_id(user_id:int):
     return {"user_id":user_id}
+
+
+
+
